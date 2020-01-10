97f9dfb1819450b6ee4a427542d265ab40badc4d
PCMBarber <Kochab.96@gmail.com>
2020-01-10 13:42:40 +0000

    Back end done

---
+++ backend/application/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, redirect, url_for, Response, request
+import random
+from application import app
+
+@app.route('/')
+@app.route('/back_end', methods=['GET','POST'])
+def back_end(json):
+
+    dice=json["Dice"]
+    char=json["Char"]
+    skills=[char["strength"],char["dexterity"],char["constitution"],char["intelligence"],char["wisdom"],char["charisma"]]
+    sort=[]
+    for skill in skills:
+        for key in dice:
+            if key == skill:
+                sort.append(dice[key])
+    char["strength"]=sort[0]
+    char["dexterity"]=sort[1]
+    char["constitution"]=sort[2]
+    char["intelligence"]=sort[3]
+    char["wisdom"]=sort[4]
+    char["charisma"]=sort[5]
+    return char
--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, redirect, url_for, Response, request
 from flask_login import login_user, current_user, logout_user, login_required
 from application import app, db, password_hash as pw
-from application.forms import LoginForm, NewChar1, NewChar2, PasswordForm
+from application.forms import LoginForm, NewChar1, NewChar2, PasswordForm, CreatePasswordForm
 from application.models import user, background, feat
 
 @app.route('/')
 @app.route('/home')
 def home():
-    query = user.query.filter_by(char_name).all()
-	return render_template('home.html', title='D&D Gen', char_names=query)
+    query = user.query.all()
+    return render_template('home.html', title='D&D Gen', users=query)
+
 
 @app.route('/login/<char_name>', methods=['GET','POST'])
 def login(char_name):
@@ -32,44 +33,78 @@ def logout():
 	logout_user()
 	return redirect(url_for('home'))
 
-@app.route('/new-character', methods=['GET','POST'])
+@app.route('/new_char', methods=['GET','POST'])
 def new_char():
-    form=NewChar1
+    form=NewChar1()
+
+    if form.validate_on_submit():
+        char_name=form.char_name.data
+        race=form.race.data
+        char_class=form.char_class.data
+        return redirect(url_for('new_char2', char_name=char_name, race=race, char_class=char_class))
+
+    return render_template('new_char1.html', title='New Character', form=form)
+
+@app.route('/new_char2/<char_name>/<race>/<char_class>', methods=['GET','POST'])
+def new_char2(char_name, race, char_class):
+    form=NewChar2()
     new_char={}
     if form.validate_on_submit():
-        char_name=form.char_name
-        race=form.race
-        char_class=form.char_class
-        form2=NewChar2
-        if form2.validate_on_submit():
-            strength=form2.strength
-            dexterity=form2.dexterity
-            constitution=form2.constitution
-            intelligence=form2.intelligence
-            wisdom=form2.wisdom
-            charisma=form2.charisma
+        strength=form.strength.data
+        dexterity=form.dexterity.data
+        constitution=form.constitution.data
+        intelligence=form.intelligence.data
+        wisdom=form.wisdom.data
+        charisma=form.charisma.data
 
-            new_char={"char_name"=char_name, "race"=race, "char_class"=char_class,
-            "strength"=strength, "dexterity"=dexterity, "constitution"=constitution,
-            "intelligence"=intelligence, "wisdom"=wisdom, "charisma"=charisma, "feats"=""}
+        new_char={"char_name":char_name, "race":race, "char_class":char_class,
+        "strength":strength, "dexterity":dexterity, "constitution":constitution,
+        "intelligence":intelligence, "wisdom":wisdom, "charisma":charisma, "feats":""} 
+        return new_char
+        #return redirect(url_for('feats', char=new_char))
+    return render_template('new_char2.html', title='New Character', form=form)
 
-            return redirect(url_for('feats', char=new_char))
-        return render_template('new_char2.html', title='New Character', form=form2)
-    return render_template('new_char1.html', title='New Character', form=form)
 
 @app.route('/feats/<char>')
 def feats(char):
-    query = feat.query.filter_by(id, name, effects).all()
+    query = feat.query.all()
     return render_template('feats.html', title='Feats', feats=query, character=char)
 
 @app.route('/submit/<feat>/<character>', methods=['GET','POST'])
 def submit(feat, character):
-    character.feats=feat
-    skill_dice=request.post('http://service1:5001/')
-    background=request.post('http://service2:5002/')
-    #random roll apps
-    #send to back end
+    
+    character['feats']=feat
+    skill_dice=request.post('http://service1:5001/') #{"1":19,"2":16,"3":10,"4":7,"5":5,"6":4}
+    background=request.post('http://service2:5002/') #{"Background":"Noble"}
 
+    request={"Char":character,"Dice":skill_dice}
+
+    char_complete=request.post=('http://backend:5003/',json=request)
+
+    form = CreatePasswordForm()
+    if form.validate_on_submit():
+        hashed = pw.hash_password(form.password.data)
+        user = user(
+            char_name=char_complete["char_name"],
+            race=char_complete["race"],
+            char_class=char_complete["char_class"],
+            strength=char_complete["strength"],
+            dexterity=char_complete["dexterity"],
+            constitution=char_complete["constitution"],
+            intelligence=char_complete["intelligence"],
+            wisdom=char_complete["wisdom"],
+            charisma=char_complete["charisma"],
+            background=background["Background"],
+            feats=char_complete["feats"],
+            password=hashed,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    else:
+        print(CreatePasswordForm.errors)
+        return render_template('Password.html', title='Password', form=form)
+    #send to back end
     #redirect to /character
 
 @app.route('/character', methods=['GET','POST'])
