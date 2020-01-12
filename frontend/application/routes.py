cd1edd3023e09fbdf59409a5a2c8a8edd0fe9f09
Piers Barber <Kochab.96@Gmail.com>
2020-01-12 18:23:05 +0000

    Jenkins Automation

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -46,13 +46,13 @@ def new_char():
 
     return render_template('new_char1.html', title='New Character', form=form)
 
-def new_char():
-    global new_char
+def new_char_var():
+    new_char={}
 
 @app.route('/new_char2/<char_name>/<race>/<char_class>', methods=['GET','POST'])
 def new_char2(char_name, race, char_class):
     form=NewChar2()
-    new_char()
+    new_char_var()
     if form.validate_on_submit():
         strength=form.strength.data
         dexterity=form.dexterity.data
@@ -61,7 +61,7 @@ def new_char2(char_name, race, char_class):
         wisdom=form.wisdom.data
         charisma=form.charisma.data
 
-        new_char={"char_name":char_name, "race":race, "char_class":char_class,
+        new_char_var.new_char={"char_name":char_name, "race":race, "char_class":char_class,
         "strength":strength, "dexterity":dexterity, "constitution":constitution,
         "intelligence":intelligence, "wisdom":wisdom, "charisma":charisma, "feats":""} 
         return redirect(url_for('feats'))
@@ -75,8 +75,8 @@ def feats():
 
 @app.route('/submit/<feat>', methods=['GET','POST'])
 def submit(feat):
-    new_char()
-    character = new_char
+    new_char_var()
+    character = new_char_var.new_char
     character['feats']=feat
     skill_dice=requests.post('http://service1:5001/') #{"1":19,"2":16,"3":10,"4":7,"5":5,"6":4}
     background=requests.post('http://service2:5002/') #{"Background":"Noble"}
