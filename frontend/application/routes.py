db0dcff9229c8b087383e3596d717bf0f1caab57
Piers Barber <Kochab.96@Gmail.com>
2020-01-12 20:39:17 +0000

    Jenkins Automation

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -80,7 +80,7 @@ def submit(feat, char_name, race, char_class, strength, dexterity, constitution,
     request=json.dumps({"Char":character,"Dice":skill_dice})
     print(request)
 
-    char_complete=requests.get('http://backend:5003/', json={"char_name":char_name,"race":race,"char_class":char_class,
+    char_complete=requests.post('http://backend:5003/', json={"char_name":char_name,"race":race,"char_class":char_class,
         "strength":strength, "dexterity":dexterity,"constitution":constitution,
         "intelligence":intelligence,"wisdom":wisdom,"charisma":charisma,"feats":feat, 
         "1":skill_dice["1"],"2":skill_dice["2"],"3":skill_dice["3"],"4":skill_dice["4"],"5":skill_dice["5"],"6":skill_dice["6"]
